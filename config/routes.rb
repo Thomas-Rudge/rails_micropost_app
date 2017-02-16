89b71a915169b6d479611427b6f14002099ac928
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-02-16 10:44:50 +0000

    Created activations controller and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   resources :users, except: [:new, :create, :update]
+  resources :account_activations, only: [:edit]
 
-  get   '/signup',   to: 'users#new'
-  post  '/signup',   to: 'users#create'
+  get   '/signup',    to: 'users#new'
+  post  '/signup',    to: 'users#create'
   patch '/users/:id', to: 'users#update', as: 'update_user'
 
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   root 'static_pages#home'
 end
