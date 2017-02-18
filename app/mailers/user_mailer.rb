3cbcfef2dff6b697429d100e30189021c3595acb
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-02-18 13:55:59 +0000

    Added password reset functionality

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Sample App account activation"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Sample App password reset request"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   NAME_REGEX  = /[\t|\r|\n|\f]+/m
@@ -36,8 +36,8 @@ class User < ApplicationRecord
   end
 
   def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(self.remember_token))
+    @remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(@remember_token))
   end
 
   def forget
@@ -59,6 +59,27 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def create_reset_digest
+    @reset_token = User.new_token
+    update_columns(reset_digest:  User.digest(@reset_token),
+                   reset_sent_at: Time.zone.now)
+  end
+
+  def clear_password_reset
+    @reset_token = nil
+    update_columns(reset_digest: nil,
+                   reset_sent_at: nil)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    return true if self.reset_sent_at.nil?
+    self.reset_sent_at < 2.hours.ago
+  end
+
   private
 
     def downcase_email
@@ -66,7 +87,7 @@ class User < ApplicationRecord
     end
 
     def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(self.activation_token)
+      @activation_token  = User.new_token
+      self.activation_digest = User.digest(@activation_token)
     end
 end
