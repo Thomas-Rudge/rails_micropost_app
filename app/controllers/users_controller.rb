fdae2c1d2d0e79c0c830f5038616428eb8e0ae06
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-03-27 18:59:26 +0100

    Updated controllers and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,14 +70,6 @@ class UsersController < ApplicationController
 
     # B E F O R E   F I L T E R S
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
